d83b574af6591f37a31f92ea0fcc773784958a63
Finn Lippok <contact@lippok.eu>
2025-12-18 22:57:08 +0100

    fix: remove unused unifi_port_profile data blocks from infrastructure.tf

--- 01-network/infrastructure.tf
+++ 01-network/infrastructure.tf
@@ -1,11 +1,3 @@
-data "unifi_port_profile" "disabled" {
-  name = "Disabled"
-}
-
-data "unifi_port_profile" "all" {
-  name = "All"
-}
-
 resource "unifi_device" "tf_cgu" {
   name = "tf-Cloud-Gateway-Ultra"
   mac  = "28:70:4e:3e:fb:15"
