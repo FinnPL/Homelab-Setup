832ba1337e94e067ef6250aed207821a44c001c6
Finn Lippok <contact@lippok.eu>
2025-12-18 23:03:24 +0100

    fix: update unifi_device port_profile_id to use data source and add local_dns_record to unifi_user

--- 01-network/infrastructure.tf
+++ 01-network/infrastructure.tf
@@ -1,3 +1,8 @@
+data "unifi_port_profile" "athena_profile" {
+  name       = unifi_network.tf_vlan_athena.name
+  depends_on = [unifi_network.tf_vlan_athena]
+}
+
 resource "unifi_device" "tf_cgu" {
   name = "tf-Cloud-Gateway-Ultra"
   mac  = "28:70:4e:3e:fb:15"
@@ -5,6 +10,6 @@ resource "unifi_device" "tf_cgu" {
   port_override {
     number          = 2
     name            = "tf-Port2"
-    port_profile_id = unifi_network.tf_vlan_athena.id
+    port_profile_id = data.unifi_port_profile.athena_profile.id
   }
 }
