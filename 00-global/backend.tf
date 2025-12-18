c5117197625e8c9ef71cfe17317e4d72760dea55
Finn Lippok <contact@lippok.eu>
2025-12-18 15:54:37 +0100

    tf s3 bucket

---
+++ 00-global/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "finnpl-homelab-tfstate-1766068376"
+    key          = "00-global/terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
