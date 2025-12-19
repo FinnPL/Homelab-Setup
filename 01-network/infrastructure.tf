3fbf6b5bed6617b6b774e20df2a03747f67545b0
Finn Lippok <contact@lippok.eu>
2025-12-19 09:48:11 +0100

    fix

--- 01-network/infrastructure.tf
+++ 01-network/infrastructure.tf
@@ -1,20 +1,3 @@
-# Temporary lookup
-data "unifi_port_profile" "search_athena" {
-  name = "Athena (Terraform)"
-}
-
-data "unifi_port_profile" "search_default" {
-  name = "Default (Terraform)"
-}
-
-output "FOUND_ATHENA_ID" {
-  value = data.unifi_port_profile.search_athena.id
-}
-
-output "FOUND_DEFAULT_ID" {
-  value = data.unifi_port_profile.search_default.id
-}
-
 resource "unifi_port_profile" "athena_custom_profile" {
   name = "Athena (Terraform)"
   site = "default"
@@ -61,34 +44,4 @@ resource "unifi_device" "tf_cgu" {
     port_profile_id = unifi_port_profile.athena_custom_profile.id
   }
   # Leave Port4 as default (Allow All VLANs)
-}
-
-resource "unifi_device" "tf_flex_mini" {
-  name = "Flex Mini"
-  mac  = "28:70:4e:32:53:14"
-  site = "default"
-  lifecycle {
-    prevent_destroy = true
-  }
-  # Leave Port1 as default (Allow All VLANs)
-  port_override {
-    number          = 2
-    name            = "tf-Port2"
-    port_profile_id = unifi_port_profile.default_custom_profile.id
-  }
-  port_override {
-    number          = 3
-    name            = "tf-Port3"
-    port_profile_id = unifi_port_profile.athena_custom_profile.id
-  }
-  port_override {
-    number          = 4
-    name            = "tf-Port4"
-    port_profile_id = unifi_port_profile.athena_custom_profile.id
-  }
-  port_override {
-    number          = 5
-    name            = "tf-Port5"
-    port_profile_id = unifi_port_profile.athena_custom_profile.id
-  }
 }
