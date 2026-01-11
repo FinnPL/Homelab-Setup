5b38e486dcc9e2eff655aef3c0435d2cef544604
Finn Lippok <contact@lippok.eu>
2026-01-11 01:56:49 +0100

    disk fix

--- 02-infrastructure/vm-windows.tf
+++ 02-infrastructure/vm-windows.tf
@@ -48,20 +48,15 @@ resource "proxmox_virtual_environment_vm" "windows_server" {
     ssd          = true
   }
 
-  # VirtIO drivers ISO on ide3
-  disk {
-    datastore_id = var.proxmox_iso_storage
-    file_id      = proxmox_virtual_environment_download_file.virtio_drivers.id
-    interface    = "ide3"
-    file_format  = "raw"
-  }
-
   # Windows installation ISO
   cdrom {
     enabled   = true
     file_id   = "${var.proxmox_iso_storage}:iso/windows-server-2025.iso"
     interface = "ide2"
   }
+
+  # VirtIO drivers ISO as secondary CD-ROM via QEMU args
+  kvm_arguments = "-drive file=/var/lib/vz/template/iso/virtio-win-stable.iso,media=cdrom,if=none,id=drive-ide3 -device ide-cd,bus=ide.3,drive=drive-ide3"
 
   scsi_hardware = "virtio-scsi-single"
 
