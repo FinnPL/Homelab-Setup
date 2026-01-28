5c713b15365b4b9a087a0444e17c68000a651f96
Finn Lippok <contact@lippok.eu>
2026-01-28 21:19:01 +0100

    Fix repository URL for csi-driver-nfs Helm chart

--- 03-services/storage.tf
+++ 03-services/storage.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "csi_driver_nfs" {
   name       = "csi-driver-nfs"
-  repository = "https://raw.githubusercontent.com/kubernetes-sigs/csi-driver-nfs/master/charts"
+  repository = "https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts" 
   chart      = "csi-driver-nfs"
   version    = "4.12.0"
   namespace  = "kube-system"
