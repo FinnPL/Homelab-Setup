8c8cc1ffa4ea1ba8f4059100a66a5699b58a94e5
Finn Lippok <contact@lippok.eu>
2026-01-30 19:58:58 +0100

    Refactor argocd Helm release configuration to use values block for Redis and applicationSet settings

--- 03-services/argocd.tf
+++ 03-services/argocd.tf
@@ -12,23 +12,19 @@ resource "helm_release" "argocd" {
   namespace  = kubernetes_namespace_v1.argocd.metadata[0].name
   timeout    = 600
 
-  set = [
-    {
-      name  = "redis.persistence.enabled"
-      value = "true"
-    },
-    {
-      name  = "redis.persistence.storageClass"
-      value = "nfs-client"
-    },
-    {
-      name  = "redis.persistence.size"
-      value = "1Gi"
-    },
-    {
-      name  = "applicationSet.enabled"
-      value = "true"
-    }
+  values = [
+    yamlencode({
+      redis = {
+        persistence = {
+          enabled      = true
+          storageClass = "nfs-client"
+          size         = "1Gi"
+        }
+      }
+      applicationSet = {
+        enabled = true
+      }
+    })
   ]
 
   depends_on = [kubernetes_storage_class_v1.nfs]
