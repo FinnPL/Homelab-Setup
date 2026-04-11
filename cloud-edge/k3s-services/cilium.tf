3b4fdb51a4acbf69dd120fc029c1394dbffbe310
Finn Lippok <contact@lippok.eu>
2026-04-11 23:23:35 +0200

    Update cilium.tf

--- cloud-edge/k3s-services/cilium.tf
+++ cloud-edge/k3s-services/cilium.tf
@@ -13,7 +13,6 @@ resource "helm_release" "cilium" {
       kubeProxyReplacement = "true"
       k8sServiceHost       = "127.0.0.1"
       k8sServicePort       = 6443
-      MTU                  = 1370
       devices              = "eth+ wg+"
       ipam = {
         mode = "kubernetes"
