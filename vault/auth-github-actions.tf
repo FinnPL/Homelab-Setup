7651461d2ba74959c91ad39245e2c1456ed03738
Finn Lippok <contact@lippok.eu>
2026-07-27 14:11:32 +0200

    feat(vault): update AWS credentials handling to use Vault for state-backend credentials and add new role configurations

--- vault/auth-github-actions.tf
+++ vault/auth-github-actions.tf
@@ -19,7 +19,47 @@ resource "vault_jwt_auth_backend_role" "vault_deploy" {
     environment      = "vault"
   }
 
-  token_policies = [vault_policy.vault_deploy.name]
+  token_policies = [vault_policy.vault_deploy.name, vault_policy.aws_tfstate.name]
+  token_type     = "service"
+  token_ttl      = 1200
+  token_max_ttl  = 1200
+}
+
+resource "vault_jwt_auth_backend_role" "vieta_layers" {
+  backend         = vault_jwt_auth_backend.github_actions.path
+  role_name       = "vieta-layers"
+  role_type       = "jwt"
+  user_claim      = "job_workflow_ref"
+  bound_audiences = [var.vault_address]
+
+  bound_claims_type = "glob"
+  bound_claims = {
+    repository       = var.github_repository
+    job_workflow_ref = "${var.github_repository}/.github/workflows/_terraform-layer.yaml@*"
+    environment      = "Terraform"
+  }
+
+  token_policies = [vault_policy.aws_tfstate.name]
+  token_type     = "service"
+  token_ttl      = 1200
+  token_max_ttl  = 1200
+}
+
+resource "vault_jwt_auth_backend_role" "cloud_edge" {
+  backend         = vault_jwt_auth_backend.github_actions.path
+  role_name       = "cloud-edge"
+  role_type       = "jwt"
+  user_claim      = "job_workflow_ref"
+  bound_audiences = [var.vault_address]
+
+  bound_claims_type = "glob"
+  bound_claims = {
+    repository       = var.github_repository
+    job_workflow_ref = "${var.github_repository}/.github/workflows/cloud-edge-deploy.yaml@*"
+    environment      = "Terraform"
+  }
+
+  token_policies = [vault_policy.aws_tfstate.name]
   token_type     = "service"
   token_ttl      = 1200
   token_max_ttl  = 1200
