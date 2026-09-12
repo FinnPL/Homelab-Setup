98f1fcae403a280dcbdbddf6be859a54f20f9ece
Finn Lippok <contact@lippok.eu>
2026-09-12 10:53:21 +0200

    fix(ci): fix renovate comments and fix image reference for myip container

--- cloud-edge/nixos/hosts/oracle-edge/myip.nix
+++ cloud-edge/nixos/hosts/oracle-edge/myip.nix
@@ -14,7 +14,7 @@
     backend = "podman";
     containers.myip = {
       # renovate: datasource=docker depName=jason5ng32/myip
-      image = "jason5ng32/myip:v7.3.0@sha256:2ff85c6bba53cf5f83f04388f545cdb32729794bf6c557279f6bdfa1428056e8";
+      image = "docker.io/jason5ng32/myip:v7.3.0@sha256:2ff85c6bba53cf5f83f04388f545cdb32729794bf6c557279f6bdfa1428056e8";
       autoStart = true;
       ports = ["127.0.0.1:18966:18966"];
       environment = {
